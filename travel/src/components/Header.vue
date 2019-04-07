b4e33a88ff13e6454d66e33cc37956dc03f1c531
Yikhan <yhxiaoch@gmail.com>
2019-04-07 22:07:58 +1000

    home page banner

---
+++ travel/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <div class="header-left">return</div>
+    <div class="header-input">City/Tour/Destination</div>
+    <div class="header-right">city</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+  
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display flex
+    line-height .86rem
+    background #00bcd4
+    color #fff
+    .header-left
+      width .64rem
+      float left
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+    .header-input
+      flex 1
+      margin-top  .12rem
+      margin-left .2rem
+      line-height .64rem
+      height .64rem
+      background  #fff
+      border-radius  .1rem
+      color #ccc
+</style>
--- travel/src/views/Home.vue
+++ travel/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    Hello
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeHeader from '@/components/Header.vue'
 
 export default {
   name: 'home',
   components: {
-  
+    HomeHeader
   }
 }
 </script>
