9d07bc7fd0928db8e94787a08ba4e8c8613aa70e
Yikhan <yhxiaoch@gmail.com>
2019-04-13 22:42:53 +1000

    add swiper to icons

--- travel/src/components/Icons.vue
+++ travel/src/components/Icons.vue
@@ -1,20 +1,92 @@
 <template>
-  <div class="icons">
-    <div class="icon">
-      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      <p class="icon-text">热门景点</p>
+ <swiper>
+   <swiper-slide v-for="(page, index) of pages" :key="index">
+    <div class="icons">
+      <div class="icon"  
+      v-for="item of page" 
+      :key="item.id" 
+      >
+        <img class="icon-img" :src="item.imgUrl">
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>     
     </div>
-  </div>
+  </swiper-slide>
+</swiper>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "Ticket"
+        },
+        {
+          id: '0002',
+          imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/f0f00d6dfe038c044dbc9a437f58b0eb.png",
+          desc: "Day Tour"
+        },
+        {
+          id: '0003',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          desc: "Peking Site"
+        },
+        {
+          id: '0004',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png",
+          desc: "Kids"
+        },
+        {
+          id: '0005',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "Ticket"
+        },
+        {
+          id: '0006',
+          imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/0334cf5430b9b5505fd79e2b8d7e8670.png",
+          desc: "Day Tour"
+        },
+        {
+          id: '0007',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          desc: "Peking Site"
+        },
+        {
+          id: '0008',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png",
+          desc: "Kids"
+        },
+         {
+          id: '0009',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "More"
+        },
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach( (item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item) 
+      } )
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/variables.styl'
+  @import '~styles/mixins.styl'
   .icons
     display flex
     flex-wrap wrap
@@ -28,8 +100,12 @@ export default {
       width 25%
       height 50%
       .icon-img
-        width 80%
-        height 80%
-      .icon-text
+        width 60%
+        height 60%
+      .icon-desc
+        width 90%
         color $darkTextColor
+        text-align center
+        ellipsis()
+        
 </style>
