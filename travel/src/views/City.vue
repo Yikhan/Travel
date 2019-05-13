d836ce29fde408b1eadabc1b2ac264f13c46e29f
Yikhan <yhxiaoch@gmail.com>
2019-05-13 22:31:49 +1000

    add city options

--- travel/src/views/City.vue
+++ travel/src/views/City.vue
@@ -2,17 +2,20 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
 import CityHeader from '@/components/City/CityHeader'
 import CitySearch from '@/components/City/CitySearch'
+import CityList from '@/components/City/CityList'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   }
 }
 </script>
